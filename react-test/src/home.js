d0d4262af38ff2935bc84144cf22c1d7e7ceb543
trv893 <trevorhilimire@gmail.com>
2022-07-06 16:47:21 -0400

    finish nav style

--- react-test/src/home.js
+++ react-test/src/home.js
@@ -1,7 +1,21 @@
+import { useState } from 'react';
+
 const Home = () => {
+    const [blogs, setName] = useState([
+        { title: "sfgbs", body: 'sakjdbjkasf', author: 'thomas', id: 1 },
+        { title: "fghfghsfgbs", body: 'bbbbsakjdbjkasf', author: 'sam', id: 2 },
+        { title: "retrtrsfgbs", body: 'zzzsakjdbjkasf', author: 'jake', id: 3 }
+    ]);
     return ( 
         <div className="home">
             <h2>Home Page</h2>
+            {blogs.map((b) => (
+                <div className="blog-preview" key={b.id}>
+                <h2> {b.title} </h2>
+                <p>written by: { b.author } </p>
+                </div>
+
+            ))}
         </div>
      );
 }
--- react-test/src/index.js
+++ react-test/src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap-icons/font/bootstrap-icons.css'
+import './navbar.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
