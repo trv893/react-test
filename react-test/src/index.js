985de11c6baaa24990c0612690188766266cf0eb
trv893 <trevorhilimire@gmail.com>
2022-07-06 22:45:47 -0400

    test

--- react-test/src/index.js
+++ react-test/src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/bootstrap-icons/font/bootstrap-icons.css'
-import './navbar.css'
+import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
+import './navbar.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
